04490ce37d2266e964a8e96cf6ff4020eef7e788
AdamAksel <adamcarlssoncoding@gmail.com>
2023-03-31 13:29:52 +0200

    fixed the preliminary signup page

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,12 +8,17 @@ import User from "../models/userSchema.js"
     }
 
     export const signup_post = async (req, res) => {
-        try { 
+        
             const { userName, email, password } = req.body;
-            const user = await User.create({ userName, email, password });
-            res.status(201).json(user);
-        } catch (error) {
-            console.log(error)
-            res.status(500).json({ message: error.message });
-        }
+            const checkEmail = await User.findOne({email: email})
+            const checkName = await User.findOne({userName: userName})
+            if(checkEmail){
+                res.send("E-mail")
+            } else if (checkName){
+                res.send("Name")
+            } else {
+                const user = await User.create({ userName, email, password });
+                res.status(201).json(user);
+            }
+       
     }
--- frontend/src/components/signup/Signup.jsx
+++ frontend/src/components/signup/Signup.jsx
@@ -6,25 +6,35 @@ const Signup = () => {
 const [userName, setUserName] = useState("")
 const [password, setPassword] = useState("");
 const [email, setEmail] = useState("");
+const [error, setError] = useState(false)
 
-function handleSignup() {
- axios
+async function handleSignup() {
+  
+await axios
     .post("http://localhost:3333/users/signup", {
       userName: userName,
       email: email,
       password: password,
     })
-    .then(function (response) {
-      console.log(response);
+    .then((res) => {
+      if(res.status === 200){
+       setError(res.data)
+        
+      } else {
+        setError(false)
+        location.assign("/")
+      }
     })
     .catch((error) => {
-      throw error;
-    });
+      console.log(error);
+      console.log(".catch")
+    })
+    
 }
 
   return (
     <>
-      <form className="signup-form">
+     <div className="signup-form">
         <h2>Signup</h2>
         <label htmlFor="userName">Username</label>
         <input onChange={(e) => {setUserName(e.target.value)}}  type="text" required id="userName" />
@@ -33,9 +43,10 @@ function handleSignup() {
         <label htmlFor="password">Password</label>
         <input onChange={(e) => {setPassword(e.target.value)}}  type="text" required id="password" />
         <button className="submit" onClick={() => {handleSignup()}}>Submit</button>
-        </form>
+        {error ? <h3>Could not create user, change {error}</h3> : null}
+        </div>
     </>
   );
 };
 
-export default Signup;
+export default Signup
