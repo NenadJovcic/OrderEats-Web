193ebdba9108e1aac2569d61eeb85210a17c500b
AdamAksel <adamcarlssoncoding@gmail.com>
2023-04-04 13:26:05 +0200

    minor fixes to classnames and dependencies

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,19 +1,26 @@
 import "../../styles/navbar.css";
 import { NavLink } from "react-router-dom";
 
-
 const Navbar = () => {
   return (
     <>
       <nav className="nav-bar">
         <div className="nav-half">
           <h1 className="nav-logo">Food Delivery</h1>
-          <NavLink className="button" to="/">Menu</NavLink>
+          <NavLink className="nav-button" to="/">
+            Menu
+          </NavLink>
         </div>
         <div className="nav-half">
-          <NavLink className="button" to="/login">Login</NavLink>
-          <NavLink className="button" to="/signup">Signup</NavLink>
-          <NavLink className="button" to="/orderuser">Order</NavLink>
+          <NavLink className="nav-button" to="/login">
+            Login
+          </NavLink>
+          <NavLink className="nav-button" to="/signup">
+            Signup
+          </NavLink>
+          <NavLink className="nav-button" to="/orderuser">
+            Order
+          </NavLink>
         </div>
       </nav>
     </>
