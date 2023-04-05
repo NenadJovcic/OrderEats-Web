56180160c92f6a06820b4bf8d0b907eeaa4c508c
NenadJovcic <nenad.hif@hotmail.se>
2023-04-05 11:09:19 +0200

    login refactor into smaller components

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,7 +1,15 @@
 import "../../styles/navbar.css";
 import { NavLink } from "react-router-dom";
 
+
 const Navbar = () => {
+
+
+  function handleLogout() {
+    localStorage.removeItem("auth-token");
+    location.assign('/login')
+
+  }
   return (
     <>
       <nav className="nav-bar">
@@ -12,15 +20,25 @@ const Navbar = () => {
           </NavLink>
         </div>
         <div className="nav-half">
-          <NavLink className="nav-button" to="/login">
-            Login
-          </NavLink>
-          <NavLink className="nav-button" to="/signup">
-            Signup
-          </NavLink>
-          <NavLink className="nav-button" to="/orderuser">
-            Order
-          </NavLink>
+          {localStorage.getItem("auth-token") ? (
+            <>
+              <button className="nav-button" onClick={handleLogout}>
+                Logout
+              </button>
+              <NavLink className="nav-button" to="/orderuser">
+                Order
+              </NavLink>
+            </>
+          ) : (
+            <>
+              <NavLink className="nav-button" to="/login">
+                Login
+              </NavLink>
+              <NavLink className="nav-button" to="/signup">
+                Signup
+              </NavLink>
+            </>
+          )}
         </div>
       </nav>
     </>
