9a7e997783aef2c1c3e926d2c22340451fbde1d7
AdamAksel <adamcarlssoncoding@gmail.com>
2023-04-05 13:03:35 +0200

    menu admin frontend mostly done

--- frontend/src/components/menuadmin/MenuAdmin.jsx
+++ frontend/src/components/menuadmin/MenuAdmin.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState } from "react";
+import "../../styles/menuadmin.css";
 
 const MenuAdmin = () => {
-  return (
-    <h1>MenuAdmin</h1>
-  )
-}
+  const [menu, setMenu] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+  const [currentItem, setCurrentItem] = useState(null);
 
-export default MenuAdmin
+  function handleMenuItem(item) {
+    setCurrentItem(item);
+  }
+
+  return (
+    <>
+      <div className="menuadmin-area">
+        <div className="menuadmin-menu">
+          {menu.map((item) => {
+            return (
+              <div
+                key={item}
+                onClick={() => {
+                  handleMenuItem(item);
+                }}
+                className="menuadmin-menu-item"
+              >
+                <img
+                  className="menuadmin-menu-item-image"
+                  src="https://healthix.org/wp-content/uploads/2016/06/testimage.jpeg"
+                  alt="test"
+                />
+                <div>
+                  <h3>Name: {item}</h3>
+                  <h4>Price: Free!</h4>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="menuadmin-editor">
+          <div>
+            <label htmlFor="menuadmin-editor-image">Image-src</label>
+            <input id="menuadmin-editor-image" placeholder={currentItem} />
+            <label htmlFor="menuadmin-editor-name">Name</label>
+            <input id="menuadmin-editor-name" placeholder={currentItem} />
+            <label htmlFor="menuadmin-editor-price">Price</label>
+            <input id="menuadmin-editor-price" placeholder={currentItem} />
+          </div>
+          <div className="menuadmin-editor-buttons">
+            <div className="menuadmin-editor-button">Put</div>
+            <div className="menuadmin-editor-button">Post</div>
+            <div className="menuadmin-editor-button">Delete</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MenuAdmin;
