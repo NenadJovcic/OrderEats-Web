b890e05fffacce0e8a2b879b4cb0eff9e6d16605
Daivd-refai <david.refai@hotmail.com>
2023-04-12 22:10:44 +0200

    I've refactored the code so it's easier to read and understand

--- frontend/src/components/orderuser/OrderUser.jsx
+++ frontend/src/components/orderuser/OrderUser.jsx
@@ -2,48 +2,53 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const OrderUser = () => {
-  const [data, setData] = useState([]);
   const [cartItems, setCartItems] = useState([]);
 
   useEffect(() => {
-    let user = localStorage.getItem("user");
-    user = JSON.parse(user);
-    const userId = user._id;
+  const fetchOrders = async () => {
+    let flatten 
+    try {
+      const user = JSON.parse(localStorage.getItem("user"));
+      const userId = user._id;
 
-    // stop rendering the data after the first time
-    const abortController = new AbortController();
-    axios
-      .get(`http://localhost:3333/orders/${userId}`, {
+      const response = await axios.get(`http://localhost:3333/orders/${userId}`, {
         headers: {
           "auth-token": localStorage.getItem("auth-token"),
         },
-      })
-      .then((res) => {
-        // create property for each item in the order before mapping it
-        const flatten = res.data.order
-          .map((element) => ({
-            ...element,
-          }))
-          .reduce((acc, curr) => {
-            return [...acc, ...curr.items];
-          }, []);
-        setData(flatten);
-
-        // remove duplicates
-        const unique = [...new Set(flatten.map((item) => item._id))].map(
-          (id) => {
-            const qty = flatten.find((item) => item._id === id);
-            return {
-              ...qty,
-              qty: flatten.filter((item) => item._id === id).length,
-            };
-          }
-        );
-
-        setCartItems(unique);
+        signal: abortController.signal,
       });
-    return () => abortController.abort();
-  }, []);
+
+      const orders = response.data.order;
+       flatten = orders.flatMap((order) => order.items);
+      const uniqueItems = flatten.reduce((unique, item) => {
+        const existingItemIndex = unique.findIndex((uniqueItem) => uniqueItem._id === item._id);
+
+        if (existingItemIndex !== -1) {
+          unique[existingItemIndex].qty++;
+        } else {
+          unique.push({ ...item, qty: 1 });
+        }
+
+        return unique;
+      }, []);
+
+      setCartItems(uniqueItems);
+    } catch (error) {
+      if (!axios.isCancel(error)) {
+        console.error(error);
+      }
+    }
+  };
+
+  const abortController = new AbortController();
+
+  fetchOrders();
+
+  return () => {
+    abortController.abort();
+  };
+}, []);
+
 
   return (
     <div className="order_container">
@@ -53,7 +58,7 @@ const OrderUser = () => {
           <li key={item._id}>
             <img src={item.photo} alt={item.name} style={{ width: "100px" }} />
             <p>{item.name}</p>
-            <p>${item.price}</p>
+            <p>Price: ${item.price}</p>
             <p>Qty: {item.qty}</p>
             <p>Total: ${item.qty * item.price} </p>
           </li>
@@ -63,10 +68,9 @@ const OrderUser = () => {
           <p>Total</p>
           <p>
             $
-            {data
-              .reduce((acc, item) => acc + item.price, 0)
-              .toFixed(2)
-              .replace(/\d(?=(\d{3})+\.)/g, "$&,")}
+            {cartItems.reduce((total, item) => {
+              return total + item.qty * item.price;
+            }, 0)}
           </p>
         </li>
       </ul>
