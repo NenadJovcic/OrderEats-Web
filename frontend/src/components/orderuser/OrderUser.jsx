ba3171ed03c12191b5e63fdaa0f511f9f1bd0abe
Daivd-refai <david.refai@hotmail.com>
2023-04-16 05:33:50 +0200

    I have added a component that determines the status of the order before and after accepted

--- frontend/src/components/orderuser/OrderUser.jsx
+++ frontend/src/components/orderuser/OrderUser.jsx
@@ -2,48 +2,67 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const OrderUser = () => {
-  const [data, setData] = useState([]);
   const [cartItems, setCartItems] = useState([]);
+  const [time, setTime] = useState(0);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    let user = localStorage.getItem("user");
-    user = JSON.parse(user);
-    const userId = user._id;
+    const fetchData = async () => {
+      const abortController = new AbortController();
 
-    // stop rendering the data after the first time
-    const abortController = new AbortController();
-    axios
-      .get(`http://localhost:3333/orders/${userId}`, {
-        headers: {
-          auth_token: localStorage.getItem("auth-token"),
-        },
-      })
-      .then((res) => {
-        // create property for each item in the order before mapping it
-        const flatten = res.data.order
-          .map((element) => ({
-            ...element,
-          }))
-          .reduce((acc, curr) => {
-            return [...acc, ...curr.items];
-          }, []);
-        setData(flatten);
+      try {
+        const userId = JSON.parse(localStorage.getItem("user"))._id;
 
-        // remove duplicates
-        const unique = [...new Set(flatten.map((item) => item._id))].map(
-          (id) => {
-            const qty = flatten.find((item) => item._id === id);
-            return {
-              ...qty,
-              qty: flatten.filter((item) => item._id === id).length,
-            };
+        const response = await axios.get(
+          `http://localhost:3333/orders/${userId}`,
+          {
+            headers: {
+              auth_token: localStorage.getItem("auth-token"),
+            },
+            signal: abortController.signal, // Add signal to cancel the request on component unmount
           }
         );
+        setData(response.data.order[0]);
+        const flatten = response.data.order.reduce((acc, curr) => {
+          return [...acc, ...curr.items];
+        }, []);
+
+        const unique = [...new Set(flatten.map((item) => item._id))].map((id) => {
+          const qty = flatten.find((item) => item._id === id);
+          return {
+            ...qty,
+            qty: flatten.filter((item) => item._id === id).length,
+          };
+        });
 
         setCartItems(unique);
-      });
-    return () => abortController.abort();
+
+        // If order is accepted, start timer for 5 minutes
+        if (response.data.order[0].accepted === true) {
+          setTime(10); 
+        }
+      } catch (error) {
+        console.error(error);
+      }
+
+      return () => abortController.abort();
+    };
+
+    fetchData();
   }, []);
+
+  useEffect(() => {
+    let timer;
+    if (time > 0) {
+      const tick = () => {
+        // If order is ready, stop timer
+        setTime((prevTime) => prevTime - 1);
+      };
+      timer = setInterval(tick, 1000);
+    }
+
+    return () => clearInterval(timer); 
+  }, [time]);
 
   return (
     <div className="order_container">
@@ -55,9 +74,7 @@ const OrderUser = () => {
             <p>{item.name}</p>
             <p>${item.price}</p>
             <p>Qty: {item.qty}</p>
-            <p>
-              Total: {item.accpted ? "time" : null} ${item.qty * item.price}{" "}
-            </p>
+            <Status time={time} data={data} />
           </li>
         ))}
 
@@ -65,8 +82,8 @@ const OrderUser = () => {
           <p>Total</p>
           <p>
             $
-            {data
-              .reduce((acc, item) => acc + item.price, 0)
+            {cartItems
+              .reduce((acc, curr) => acc + curr.price * curr.qty, 0)
               .toFixed(2)
               .replace(/\d(?=(\d{3})+\.)/g, "$&,")}
           </p>
@@ -74,6 +91,36 @@ const OrderUser = () => {
       </ul>
     </div>
   );
+};
+
+// This component is used to display the status of the order (pending, ready, etc.)
+const Status = ({ time, data }) => {
+  if (data.accepted && !data.ready) {
+    if (time === 0) {
+      axios.put(`http://localhost:3333/orders/${data._id}`, {
+        ready: true,
+        },
+        {
+          headers: {
+            auth_token: localStorage.getItem("auth-token"),
+            },
+            });
+      return <p>Order Ready</p>;
+    } else {
+      const minutes = String(Math.trunc(time / 60)).padStart(2, 0);
+      const seconds = String(time % 60).padStart(2, 0);
+      return (
+        <p>
+          Order Ready in:
+          {minutes}:{seconds}
+        </p>
+      );
+    }
+  } else if (data.ready) {
+    return <p>Order Ready</p>;
+  } else {
+    return <p>Pending</p>;
+  }
 };
 
 export default OrderUser;
